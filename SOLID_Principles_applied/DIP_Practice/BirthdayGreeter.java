d7036b2aafa85c0f22fd5ef02ce299d4e90c0003
ngch43 <neil.chacon@mediaadgo.com>
2023-10-24 00:55:26 +0200

    Añadida la practica de DIP según el ejemplo Kata del curso, hicimos algunas modificaciones minimas pero realmente lo más que pudimos refactorizar a mi pareser fue la instanciación del EmailSender en el caso de uso de BirthdayGreeter

---
+++ SOLID_Principles_applied/DIP_Practice/BirthdayGreeter.java
@@ -0,0 +1,29 @@
+package com.codurance.dip;
+
+import java.time.MonthDay;
+
+public class BirthdayGreeter {
+    private final EmployeeRepository employeeRepository;
+    private final Clock clock;
+    private final EmailSender emailSender;
+
+    public BirthdayGreeter(EmployeeRepository employeeRepository, Clock clock, EmailSender emailSender) {
+        this.employeeRepository = employeeRepository;
+        this.clock = clock;
+        this.emailSender = emailSender;
+    }
+
+    public void sendGreetings() {
+        MonthDay today = clock.monthDay();
+        employeeRepository.findEmployeesBornOn(today)
+                .stream()
+                .map(employee -> emailFor(employee))
+                .forEach(email -> this.emailSender.send(email));
+    }
+
+    private Email emailFor(Employee employee) {
+        String message = String.format("Happy birthday, dear %s!", employee.getFirstName());
+        return new Email(employee.getEmail(), "Happy birthday!", message);
+    }
+
+}
